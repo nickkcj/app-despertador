53dc9bfcd476430809d1fd82c00c475f751fc21a
nicolasmichielon <nicolasmichielon@gmail.com>
2025-11-30 21:08:04 -0300

    refactor: clean up comments and improve error logging in API service

--- src/services/api.js
+++ src/services/api.js
@@ -1,14 +1,11 @@
 import axios from 'axios';
 
 
-// Para emulador Android use: 'http://10.0.2.2:3000'
-// Para dispositivo físico use o IP da sua máquina
 export const BASE_URL = 'http://192.168.15.6:3000';
 
 // ID do dispositivo ESP32
 export const DEVICE_ID = 'despertador01';
 
-// Instância do Axios configurada
 const api = axios.create({
   baseURL: BASE_URL,
   timeout: 10000,
@@ -17,7 +14,7 @@ const api = axios.create({
   },
 });
 
-// Interceptor para log de erros (opcional, útil para debug)
+// Interceptor para log de erros
 api.interceptors.response.use(
   (response) => response,
   (error) => {
